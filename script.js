8113416a9e1bea0215e46622044d8442642079d0
Vishakha0123 <109223509+Vishakha0123@users.noreply.github.com>
2025-10-30 12:42:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset-btn");
+let newGamebtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turnO=true;//playerX,playerO
+let count=0;//to track draw
+//2d array
+
+const winningPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+const resetGame=()=>{
+    turnO=true;
+    count=0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if (turnO){//playerO
+            box.innerText="O";
+            turnO=false;
+        }else {//playerX
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true;
+        count++;
+
+        let isWinner=checkWinner();
+        if(count===9 && !isWinner){
+            gameDraw();
+        }
+    });
+});
+
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulations,Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner=()=>{
+    for(let pattern of winningPatterns){
+        let position1=boxes[pattern[0]].innerText;
+        let position2=boxes[pattern[1]].innerText;
+        let position3=boxes[pattern[2]].innerText;
+        
+        if(position1 !== "" && position2 !== "" && position3 !== ""){
+            if(position1===position2 && position2===position3){
+                showWinner(position1);
+                return true;
+            }
+        }
+    }
+};
+
+
+newGamebtn.addEventListener("click",resetGame);
+resetbtn.addEventListener("click",resetGame);
+
